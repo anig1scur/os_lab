ff1d8c3a5f2d41f0c317b1ad07c4614033349413
zGina <upupgina@163.com>
2019-12-27 20:17:51 +0800

    deadlock request

--- DeadLock&Process Communication/banker.py
+++ DeadLock&Process Communication/banker.py
@@ -2,6 +2,7 @@
 # 1. x = [[foo for i in range(10)] for j in range(10)]
 # 2. import numpy
 #    matrix = numpy.empty((10, 10))
+import copy
 
 Allocation = []
 Max = []
@@ -24,6 +25,7 @@ Max = [list(map(int, input().strip().split())) for i in range(m)]
 print("输入各个资源剩余可用数目")
 Available = list(map(int, input().strip().split()))
 Need = [[Max[i][j] - Allocation[i][j] for j in range(n)] for i in range(m)]
+Available_ = copy.deepcopy(Available)
 
 """
 Proc = [p1,p2,p3,p4,p5]
@@ -33,8 +35,9 @@ Avail = [4] 5
 Max = [5][4]
 Need = [5][4]
 """
-if __name__ == "__main__":
+def get_seq():
     safe_seq = []
+    
     # 遍历两遍进程列表——>所有情况
     for k in  range(len(Proc)):
         for i in range(len(Proc)):
@@ -46,7 +49,7 @@ if __name__ == "__main__":
                     if (Need[i][j] > Available[j]):
                         secure = 0
                         break
-                # 否 Avail += Allocation
+                # 否则 Avail += Allocation
                 # 更新可用资源
                 if secure:
                     for j in range(len(Res)):
@@ -56,6 +59,43 @@ if __name__ == "__main__":
         print("不存在安全序列,以下进程不能被执行：")
         cant = [item for item in Proc if item not in safe_seq]
         print(cant)
+        return False
     else:
         print("存在安全序列如下：")
         print(safe_seq)
+        return True
+
+def compare(list1, list2):
+    for i in range(0, len(list1)):
+        if list1[i] > list2[i]:
+            return False
+    return True
+
+if __name__ == "__main__":
+    secure=True
+    get_seq()
+    while (True):
+        print("请输入请求进程及资源")
+        p = int(input())
+        request = list(map(int, input().strip().split()))
+        #### 检查1
+        if compare(request,Need[p]):
+            #### 检查2
+            if compare(request,Available_):
+                #在 分配的边缘试探
+                Available = copy.deepcopy(Available_)
+                for j in range(0,len(request)):
+                    Available[j] -= request[j]
+                    Allocation[p][j] += request[j]
+                    Need[p][j] -= request[j]
+                Available_=copy.deepcopy(Available)
+                #分配之后要安全呐！
+                if not get_seq():
+                    print("分配失败")
+            else:
+                print("尚无够用资源，等待吧！")
+        else:
+            print("请求已超过所宣布的最大值")
+          
+
+
